8fa173e7cad1b56304f660030805aa37225f4f3a
Shawon Ashraf <shawon13@live.com>
2018-06-06 03:24:35 +0600

    test seed for books

---
+++ tests/seed.js
@@ -0,0 +1,30 @@
+import { ObjectID } from "mongodb"
+import Book from "../models/book";
+
+const bookOneId = new ObjectID()
+const bookTwoId = new ObjectID()
+
+export const books = [{
+        _id: bookOneId,
+        name: "BookOne",
+        author: "AuthorOne",
+        addedOn: new Date().toTimeString(),
+        addedBy: 'A lousy cat'
+    },
+    {
+        _id: bookTwoId,
+        name: "BookTwo",
+        author: "AuthorTwo",
+        addedOn: new Date().toTimeString(),
+        addedBy: 'A lousy cat'
+    }
+]
+
+export const populateBookData = (done) => {
+    Book.remove({})
+        .then(() => {
+            Book.insertMany(books)
+        })
+        .then(() => done())
+        .catch(err => console.log(err))
+}
