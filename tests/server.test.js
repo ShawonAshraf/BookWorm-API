8707967c31d63d3519f7069cddebb11eeff11d29
Shawon Ashraf <shawon13@live.com>
2018-06-09 04:27:38 +0600

    test for get book by id

--- tests/server.test.js
+++ tests/server.test.js
@@ -15,12 +15,23 @@ beforeEach(populateBookData)
 // test
 describe("GET /books", () => {
     it("Should fetch all the books", (done) => {
-        // expetc 2 books
+        // expect 2 books
         request(app)
             .get("/books/all")
             .expect(200)
             .expect(res => {
                 expect(res.body.books.length).toBe(2)
+                done()
+            })
+            .catch(err => done(err))
+    })
+
+    it("Should fetch a book by id", (done) => {
+        request(app)
+            .get(`/books/${books[1]._id}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.books._id).toBe(books[1]._id)
                 done()
             })
             .catch(err => done(err))
