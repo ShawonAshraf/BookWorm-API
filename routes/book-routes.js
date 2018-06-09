1c61c86955eb5ea18f6fea9fed8dcea253767947
Shawon Ashraf <shawon13@live.com>
2018-06-10 01:52:49 +0600

    add a book route

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -4,7 +4,7 @@ import { ObjectID } from "mongodb"
 
 import Book from "../models/book"
 
-var bookRouter = express.Router()
+let bookRouter = express.Router()
 
 // GET all books
 bookRouter.get("/all", (req, res) => {
@@ -19,7 +19,7 @@ bookRouter.get("/all", (req, res) => {
 
 // get book by mongodb document id
 bookRouter.get("/byid/:id", (req, res) => {
-    var id = req.params.id
+    let id = req.params.id
 
     if (ObjectID.isValid(id)) {
         Book.findOne({ _id: id })
@@ -36,7 +36,7 @@ bookRouter.get("/byid/:id", (req, res) => {
 
 // get book by name
 bookRouter.get("/byname/:name", (req, res) => {
-    var name = req.params.name
+    let name = req.params.name
 
     Book.findOne({ name: name })
         .then(book => {
@@ -47,13 +47,27 @@ bookRouter.get("/byname/:name", (req, res) => {
 
 // get book by author
 bookRouter.get("/byauthor/:author", (req, res) => {
-    var author = req.params.author
+    let author = req.params.author
 
     Book.findOne({ author: author })
         .then(book => {
             res.status(200).send(book)
         })
         .catch(err => res.status(404).send(err))
+})
+
+// add a book
+bookRouter.post("/add", (req, res) => {
+    let book = req.body
+
+    let bookEntry = new Book({
+        name: book.name,
+        author: book.author,
+        addedOn: new Date(),
+        addedBy: book.addedBy
+    })
+
+    bookEntry.save().then(book => res.send(book), err => res.send(err))
 })
 
 export default bookRouter
