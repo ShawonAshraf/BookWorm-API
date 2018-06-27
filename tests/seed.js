260f72f81febb8f813f3f2cbee10e56fd49e4e9a
Shawon Ashraf <shawon13@live.com>
2018-06-27 14:56:21 +0600

    user seed

--- tests/seed.js
+++ tests/seed.js
@@ -1,22 +1,41 @@
 import { ObjectID } from "mongodb"
-import Book from "../models/book";
+import Book from "../models/book"
+import User from "../models/user"
 
 const bookOneId = new ObjectID()
 const bookTwoId = new ObjectID()
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
+
+
+export const users = [{
+        _id: userOneId,
+        name: "User1",
+        email: "user1@bookworm.com",
+        password: "123"
+    },
+    {
+        _id: userTwoId,
+        name: "User2",
+        email: "user2@bookworm.com",
+        password: "123"
+    }
+]
 
 export const books = [{
         _id: bookOneId,
         name: "BookOne",
         author: "AuthorOne",
         addedOn: new Date().toTimeString(),
-        addedBy: 'A lousy cat'
+        addedBy: users[0].name
     },
     {
         _id: bookTwoId,
         name: "BookTwo",
         author: "AuthorTwo",
         addedOn: new Date().toTimeString(),
-        addedBy: 'A lousy cat'
+        addedBy: users[1].name
     }
 ]
 
@@ -24,6 +43,14 @@ export const populateBookData = (done) => {
     Book.remove({})
         .then(() => {
             return Book.insertMany(books)
+        })
+        .then(() => done())
+}
+
+export const populateUserData = (done) => {
+    User.remove({})
+        .then(() => {
+            return User.insertMany(users)
         })
         .then(() => done())
 }
