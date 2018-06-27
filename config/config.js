d32bc0dd6a3bedafa4f8ff78d12602397d70f1d9
Shawon Ashraf <shawon13@live.com>
2018-06-27 15:32:55 +0600

    env log

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 import * as config from "./config.json"
 
 let env = process.env.NODE_ENV || "test"
+console.log("ENV: ", env)
 
 if (env === "test" || env === "development") {
     let envConfig = config[env]
