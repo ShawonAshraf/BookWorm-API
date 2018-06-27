24310b8159de3043acb094a0c560a508cca563d7
Shawon Ashraf <shawon13@live.com>
2018-06-27 22:22:26 +0600

    changed payload for auth

--- models/user.js
+++ models/user.js
@@ -47,7 +47,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
     let user = this
     let access = "auth"
-    let jwtPayload = user._id.toHexString() + user.email + user.password
+    let jwtPayload = user._id.toHexString()
     let token = jwt.sign({ _id: jwtPayload, access }, process.env.JWT_SECRET).toString()
 
     user.tokens.push({
