23c890057e333fa44e9b8e3c514c712348d4b61e
Shawon Ashraf <shawon13@live.com>
2018-06-27 18:40:56 +0600

    login validation

--- tests/user.test.js
+++ tests/user.test.js
@@ -45,24 +45,30 @@ describe("POST /user", () => {
             .catch((err) => done(err))
     })
 
-    it("should login an user and return auth token and user", (done) => {
-        let email = users[1].email
-        let password = users[1].password
-
-        let user = {
-            email: email,
-            password: password
-        }
-
+    it('should login user and return auth token', (done) => {
         request(app)
             .post("/user/login")
-            .send(user)
-            .expect(200)
-            .expect(res => {
-                expect(res.body.email).toBe(email)
-                done()
+            .send({
+                email: users[1].email,
+                password: users[1].password
             })
-            .catch(err => done(err))
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist()
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[1]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    })
+                    done()
+                }).catch((e) => done(e))
+            })
     })
 
     it("should reject invalid login", (done) => {
